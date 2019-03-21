c4e51976c2d12f703ac38b1063aac6c4056c35c5
Sadoed <barmoza999@gmail.com>
2019-03-21 23:55:31 +0300

    init the project

---
+++ accelerometer/accelerometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accelerometer
+//
+//  Created by more on 21/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
