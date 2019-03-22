298ebdd52856743a62e15121edd3b8b06e58e437
Sadoed <barmoza999@gmail.com>
2019-03-22 23:51:39 +0300

    add: view and code in progress

--- accelerometer/accelerometer/ViewController.swift
+++ accelerometer/accelerometer/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+
+    var motionManager:CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
     }
-
-
+    
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else {return}
+        print(accelerometerData)
+    }
 }
 
